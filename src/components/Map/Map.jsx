a0c7eff9ffa131d915fc59d41982349713b8391d
Rico John Dato-on <ricojohndatoon23@gmail.com>
2022-01-16 23:37:00 -0500

    Move api key to .env file

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -15,7 +15,7 @@ function Map() {
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_GOOGLE_MAP_API }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
