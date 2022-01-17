38e37b11615307a961c46e223d6b12000a523a21
Rico John Dato-on <ricojohndatoon23@gmail.com>
2022-01-16 22:17:02 -0500

    Map Component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,31 @@
+import GoogleMapReact from 'google-map-react';
+import { useMediaQuery } from '@material-ui/core';
+import {} from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
+
 function Map() {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  const coordinates = { lat: 43.75474384945263, lng: -79.31631430760713 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_GOOGLE_MAP_API }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      ></GoogleMapReact>
+    </div>
+  );
 }
 
 export default Map;
